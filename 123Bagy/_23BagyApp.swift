1f055f1135f2b036cc4df29ee58c4ac4ac238c39
insect2510 <155474669+insect2510@users.noreply.github.com>
2026-09-03 21:25:58 +0200

    Initial Commit

---
+++ 123Bagy/_23BagyApp.swift
@@ -0,0 +1,17 @@
+//
+//  _23BagyApp.swift
+//  123Bagy
+//
+//  Created by Oliver Hartmann on 03.09.26.
+//
+
+import SwiftUI
+
+@main
+struct _23BagyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
